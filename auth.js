5925a0c196f26a84e8f1ca73dd147b9fb9b3a982
root <root@localhost>
2017-01-23 10:55:09 +0300

    Changed auth on conflicted branch

--- auth.js
+++ auth.js
@@ -2,6 +2,6 @@ function authenticate (login, password) {
 	if (login === 'login' && password === 'password') {
 		return 'You wehe logged in';
 	} else {
-		return 'Login is incorrect';
+		return 'Login is incorrect!!!!';
 	}
 }
